cdb38c34fb02b43e3a8b3d6b02f14a505c82f569
Mataja Maughan <matajamaughan@gmail.com>
2020-02-12 11:09:00 -0700

    started frontend with create-react-app

--- Backend/app.js
+++ Backend/app.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 const MONGODB_URI = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0-3oedr.mongodb.net/recipe-box?retryWrites=true&w=majority`;
-const PORT = process.env.port || 8000;
+const PORT = process.env.port || 3000;
 
 const graphqlSchema = require('./graphql/schema');
 const graphqlResolver = require('./graphql/resolvers');
